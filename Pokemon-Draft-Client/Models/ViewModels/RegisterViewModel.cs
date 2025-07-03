a90667d7811730f23401fe1110c153ce7068181a
Theo <theokaminsky@gmail.com>
2025-07-03 15:38:28 +0200

    implemented account creation and password validation

---
+++ Pokemon-Draft-Client/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pokemon;
+
+public class RegisterViewModel
+{
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a Username")]
+    public string? Username { get; set; }
+    
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a Password")]
+    public string? Password { get; set; }
+    
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter the Password again")]
+    public string? RepeatPassword { get; set; }
+}
