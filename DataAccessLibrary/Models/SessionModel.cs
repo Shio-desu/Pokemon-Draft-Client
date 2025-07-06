c184157cefd74bea22abeb11b5c0e2e7050b9ede
Theo <theokaminsky@gmail.com>
2025-07-06 12:47:04 +0200

    begun session creation
    can create session entry in database now

--- DataAccessLibrary/Models/SessionModel.cs
+++ DataAccessLibrary/Models/SessionModel.cs
@@ -1,12 +1,14 @@
+using System;
+
 namespace DataAccessLibrary.Models
 {
     public class SessionModel
     {
         public int SessionId { get; set; }
-        public double StartDate { get; set; }
-        public double EndDate { get; set; }
-        public string SessionName { get; set; }
-        public string SessionType { get; set; }
+        public string? SessionName { get; set; }
+        public string? SessionType { get; set; }
         public bool HasStarted { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
     }
 }
